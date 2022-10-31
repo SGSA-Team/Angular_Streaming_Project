2467e130282a77b6ac70efdd987dda8f143cda2e
SamiDev092 <samir.hassen@loxam.fr>
2022-10-31 14:11:43 +0100

    [SDS-40] fetching data on filters update for movies and series

--- front/src/services/series.service.ts
+++ front/src/services/series.service.ts
@@ -37,4 +37,12 @@ export class SeriesService {
     return this.http.get<ApiSeries>(`${this.apiBaseUrl}/discover/tv?sort_by=first_air_date.${type}&page=${page}&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`);
   };
 
+  getPopularSeries= (type:string="desc", page:number = 1): Observable<ApiSeries> => {
+    return this.http.get<ApiSeries>(`${this.apiBaseUrl}/discover/tv?sort_by=popularity.${type}&page=${page}&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`);
+  };
+
+  getRatedSeries= (type:string="desc", page:number = 1): Observable<ApiSeries> => {
+    return this.http.get<ApiSeries>(`${this.apiBaseUrl}/discover/tv?sort_by=vote_average.${type}&page=${page}&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`);
+  };
+
 }
