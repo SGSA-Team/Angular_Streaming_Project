3f637d154ac2b2f0d99bd42ccfce5c5de7bfdaba
giovanni-nkp <59953626+giovanni-nkp@users.noreply.github.com>
2022-10-31 23:18:18 +0100

    [SDS-36] Page layout

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,13 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { CatalogsComponent } from './catalogs/catalogs.component';
 import { HomeComponent } from './home/home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'catalogs/movies', pathMatch: 'full',component: CatalogsComponent },
-  { path: 'catalogs/movies/:id', pathMatch: 'full',component: CatalogsComponent },
-  { path: 'catalogs/series', pathMatch: 'full',component: CatalogsComponent },
-  { path: 'catalogs/series/:id', pathMatch: 'full',component: CatalogsComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'catalogs/movies', pathMatch: 'full', component: CatalogsComponent },
+  {
+    path: 'catalogs/movies/:id',
+    pathMatch: 'full',
+    component: CatalogsComponent,
+  },
+  { path: 'catalogs/series', pathMatch: 'full', component: CatalogsComponent },
+  {
+    path: 'catalogs/series/:id',
+    pathMatch: 'full',
+    component: PagenotfoundComponent,
+  },
   { path: '**', redirectTo: '', component: PagenotfoundComponent },
 ];
 
