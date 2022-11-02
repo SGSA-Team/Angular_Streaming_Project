2ef349692baf4c2c1621fddc6f6641e3df6593a4
amine <amine.djennadi@hotmail.fr>
2022-11-02 19:45:10 +0100

    [SDS-44] remove lang

--- front/src/app/layout/footer/footer.component.ts
+++ front/src/app/layout/footer/footer.component.ts
@@ -7,15 +7,6 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  language = localStorage.getItem('lang') || environment.defaultLanguage;
-  propUsername: string = '';
   constructor() {}
-
-  changeLanguage(language: Lang) {
-    console.log(language);
-    localStorage.setItem('lang', language);
-  }
   ngOnInit(): void {}
 }
-
-type Lang = 'es-ES' | 'en-US' | 'fr-FR';
